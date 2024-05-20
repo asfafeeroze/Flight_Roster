2bc44a2aaa31a85e6939c3ab8c15d62bcc40ee59
asfafeeroze <115186085+asfafeeroze@users.noreply.github.com>
2024-05-20 14:27:30 +0300

    Add files via upload
    Added the code for switching between NoSQL and MySQL

--- connect.py
+++ connect.py
@@ -1,3 +1,7 @@
+import json
+import os
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData
+from sqlalchemy.orm import sessionmaker
 from pymongo import MongoClient
 
 def connectDB():
@@ -15,3 +19,52 @@ def connectDB():
     return db
     # Close the connection when you're done
     # client.close()
+
+# BELOW CODE IS FOR PROVIDING THE USER THE UTILITY TO CHOOSE BETWEEN NoSQL and MySQL
+
+# Load configuration from config.json
+# with open('config.json') as config_file:
+#     config = json.load(config_file)
+#
+# def connect_mysql():
+#     mysql_config = config['mysql']
+#     engine = create_engine(f'mysql+mysqlconnector://{mysql_config["user"]}:{mysql_config["password"]}@{mysql_config["host"]}/{mysql_config["database"]}')
+#     Session = sessionmaker(bind=engine)
+#     return engine, Session()
+#
+# def connect_nosql():
+#     mongo_config = config['mongodb']
+#     client = MongoClient(mongo_config['host'], mongo_config['port'])
+#     db = client[mongo_config['database']]
+#     return db
+#
+# def store_roster_mysql(engine, session, roster):
+#     metadata = MetaData()
+#     roster_table = Table('roster', metadata,
+#                          Column('id', Integer, primary_key=True, autoincrement=True),
+#                          Column('data', String))
+#     metadata.create_all(engine)
+#     roster_json = json.dumps(roster)
+#     conn = engine.connect()
+#     conn.execute(roster_table.insert(), {"data": roster_json})
+#     session.commit()
+#     conn.close()
+#
+# def store_roster_nosql(db, roster):
+#     roster_collection = db['rosters']
+#     roster_collection.insert_one(roster)
+#
+# def retrieve_roster_mysql(session):
+#     result = session.execute('SELECT data FROM roster')
+#     roster_data = result.fetchall()
+#     rosters = [json.loads(row['data']) for row in roster_data]
+#     return rosters
+#
+# def retrieve_roster_nosql(db):
+#     roster_collection = db['rosters']
+#     rosters = list(roster_collection.find({}))
+#     return rosters
+#
+# def export_roster_to_json(roster, file_path='roster.json'):
+#     with open(file_path, 'w') as json_file:
+#         json.dump(roster, json_file)
